488c619d6799280dc38a2f82dedd1b130ddd41a3
Emma Bindi <emma.bindi@outlook.com>
2020-05-11 14:37:00 +1000

    Added category to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_043228) do
+ActiveRecord::Schema.define(version: 2020_05_11_043604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2020_05_11_043228) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_listings_on_category_id"
   end
 
+  add_foreign_key "listings", "categories"
 end
