5cb4cbde412319b19998aebcd25f36507dd7b21d
Emma Bindi <emma.bindi@outlook.com>
2020-05-11 14:31:56 +1000

    Updated validation with length msg for listing title, drafted seed categories

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,6 @@
 class Listing < ApplicationRecord
-  validates :title, presence: true, length: 1..30
+  validates :title, presence: true
+  validates_length_of :title, within: 1..30, too_long: "Please ensure title is less than 30 characters long"
   validates :price, presence: true
   validates :qty, presence: true
 end
---
+++ db/production_seeds.rb
@@ -0,0 +1,29 @@
+# categories = ["Vegetables", "Fruit", "Herbs"]
+
+# categories.each do | cat |
+#   Categories.create(category_type: cat)
+# end
+
+# veg = ["Artichoke", "Asparagus", "Bak Choy", "Beans", "Beetroot", "Brocolli", "Brussel Sprouts", "Cabbage", "Capsicum", "Carrot", "Cauliflower", "Chilli", "Corn", "Celery", "Cucumber", "Eggplant", "Fennel", "Garlic", "Ginger", "Kale", "Kholrabi", "Leek", "Lettuce", "Mushrooms", "Okra", "Onion", "Parsnip", "Peas", "Potatos", "Pumpkin", "Radish", "Rhubarb", "Squash", "Spinach", "Tomato", "Other"]
+
+# veg.each do | veg |
+#   Categories.create(veg_type: veg)
+# end
+
+
+# anise
+# basil
+# caraway
+# coriander
+# chamomile
+# daikon
+# dill
+# fennel
+# lavender
+# lemongrass
+# marjoram
+# oregano
+# parsley
+# rosemary
+# sage
+# thyme
