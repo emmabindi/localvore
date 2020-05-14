caf5ac18ff501fb2959e1c6c9d0b4552f6b1172e
Emma Bindi <emma.bindi@outlook.com>
2020-05-14 17:48:18 +1000

    Added search by item name, filter by category and filter by type to listings index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,16 @@
 class Category < ApplicationRecord
   has_many :listings, dependent: :destroy
   has_many :subcategories
+
+  def self.search_select_options
+    Category.order("name").collect do |cat |
+      [cat.name, cat.id]
+    end
+  end
+
+  def self.search_select_options
+    category_list = Category.all.collect do |cat|
+      [cat.name, cat.name]
+    end
+  end
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,4 +1,16 @@
 class Subcategory < ApplicationRecord
   has_many :listings, dependent: :destroy
   belongs_to :category
+
+  def self.search_select_options
+    Subcategory.order("name").collect do |subcat |
+      [subcat.name, subcat.id]
+    end
+  end
+
+  def self.search_select_options
+    subcategory_list = Subcategory.all.collect do |subcat|
+      [subcat.name, subcat.name]
+    end
+  end
 end
