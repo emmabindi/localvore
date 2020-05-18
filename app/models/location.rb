49f0a7983a6e2ebf89835195f03d4bc0ecc09fdf
Emma Bindi <emma.bindi@outlook.com>
2020-05-19 08:47:28 +1000

    Removed commented code re address method

--- app/models/location.rb
+++ app/models/location.rb
@@ -11,8 +11,6 @@ class Location < ApplicationRecord
   end
 
   def full_address
-    # self.street_address + self.suburb + self.state + self.country
-    # [local_address, state, country].compact.join(', ')
     [street_address, suburb, state, country].compact.join(', ')
   end
 end
